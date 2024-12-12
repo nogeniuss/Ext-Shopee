3bde9d03af1869bc4ff16b7fa22f4661c1d565f5
nogeniuss <nogeniuss@gmail.com>
2024-12-12 18:35:33 -0300

    update js, para um codigo mais legivel e tratamentos de dados

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-setTimeout( () => {
+setTimeout(() => {
     const div = document.querySelector('.performance-title-container');
 
-    //seleciona os valores dos elementos e colocaos em um array
+    // Seleciona os valores dos elementos e coloca-os em um array
     const elementos = document.querySelectorAll('.text.ellipsis');
-    console.log('Elementos encontrados :', elementos)
+    console.log('Elementos encontrados:', elementos);
+    window.alert(elementos)
     const elementosArray = Array.from(elementos).map(element => element.textContent);
-    console.log('Elementos Array :', elementosArray);
+    console.log('Elementos Array:', elementosArray);
 
     // Adiciona o botão na tela
-    div?.insertAdjacentHTML(
-        'beforeend',
-        `
-      <div class='button-copiar'>
-      <button class='shopee-btn'>Copiar</button>
-      </div>
-  `);
+    if (div) {
+        // Cria uma div com class
+        const divbutton = document.createElement('div');
+        divbutton.className = "button-copiar";
+        window.alert('div')
 
-    // Adiciona o evento de clique ao botão
-    const button = document.querySelector('.shopee-btn');
-    button.addEventListener('click', copiar);
+        // Cria o btn
+        const buttoncopiar = document.createElement('button');
+        buttoncopiar.textContent = "Copiar"; // Nome do button
+        buttoncopiar.className = "shopee-btn"; // Class
+        window.alert('Button')
+
+        // Adiciona o button na div
+        divbutton.appendChild(buttoncopiar);
+        div.appendChild(divbutton); // Adiciona a div que contém o botão à div principal
+        window.alert('Button adicionado')
+
+        // Adiciona o evento de clique ao botão
+        buttoncopiar.addEventListener('click', copiar);
+        window.alert('eventlistener')
+    }
 
     // Função copiar
     function copiar() {
-        const conteudoParaCopiar = elementosArray.join(', ');//treinar await
+        const conteudoParaCopiar = elementosArray.join(', '); // Treinar await
 
         if (conteudoParaCopiar) {
             navigator.clipboard.writeText(conteudoParaCopiar)
@@ -38,4 +49,4 @@ setTimeout( () => {
             window.alert('Nenhum elemento encontrado para copiar');
         }
     }
-}, 7000);//tempo de espera
+}, 10000); // Tempo de espera
